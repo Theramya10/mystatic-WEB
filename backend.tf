b28ddeec12978f25abb6d789c3b1c40b623d1f88
Ramya Ranjan Sahoo <114637302+Theramya10@users.noreply.github.com>
2025-03-19 15:42:10 +0530

    backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "theramya10"
     key            = "My1stWEB/tfstate/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-lock"
     encrypt        = true
   }
 }
